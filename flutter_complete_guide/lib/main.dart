cc24006e7723c5fd5cb76e05a4e5eec9a7ce8493
Pwaeogxer <pedro.moraes@unesp.br>
2019-09-02 11:43:30 -0300

    Updates just until before "maps"

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -1,16 +1,68 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+import './answers.dart';
+
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+//gets rebuilt
+class MyApp extends StatefulWidget {
+
+  @override//decorator
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+
+}
+
+//data within does not reset when StatefulWidget is rebuilt aka is persistent
+class _MyAppState extends State<MyApp> { //State is a generic class that belongs to MyApp
+
+  //variable outside of build because it would be reset everytime it is run
+  var _questionIndex = 0;
+
+  //onPressed requires a function as argument
+  //function declared within the class because the widget must be a standalone unit
+  void _answerQuestion() {
+    setState((){
+      _questionIndex++;
+    });
+    print('Answer chosen!');
+    print('$_questionIndex');
+  }
+
   @override //decorator
   Widget build(BuildContext context) {
+    var questions = [
+      
+      
+    ];
+
     return MaterialApp(
         home: Scaffold(
-          appBar: AppBar(title: Text("An app")),
-          body: Center(
-              child: (Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  Text('Holla Holla if ya can hear me'),
-])))));}}
+      appBar: AppBar(title: Text("An app")),
+      body: Column(children: /*<Widget>*/ [
+        Question(questions[_questionIndex]),
+        Answer(_answerQuestion),
+        Answer(_answerQuestion),
+        Answer(_answerQuestion),
+        /*RaisedButton(
+          child: Text('Answer 1'),
+          onPressed: _answerQuestion,
+        ),
+        RaisedButton(
+          child: Text('Answer 2'),
+          onPressed: () => print('This is an anonymous function!'),
+        ),
+        RaisedButton(
+          child: Text('Answer 3'),
+          onPressed: () {
+            //...
+            print('This is another anonymous function!');
+          },
+        )*/
+      ]),
+    ));
+  }
+}
